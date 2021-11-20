d1d142cf8d0a457a8cd7e529c3cf298a2243d52a
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-20 16:14:44 +0200

    Osa 3, tehtävät 3.5 ja 3.6.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3001;
 
 let persons = [
@@ -25,6 +27,23 @@ let persons = [
         id: 4
       }
     ]
+
+const generateId = () => {
+    const existingIds = persons.map(person => person.id);
+    let newId = Math.floor(Math.random() * 500);
+    
+    let createNew = true;
+    while (createNew) {
+        if (!existingIds.includes(newId)) {
+            createNew = false;
+            return newId;
+        }
+        else {
+            newId = Math.floor(Math.random() * 500);
+            continue;
+        }
+    }
+}
 
 app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id);
@@ -51,13 +70,34 @@ app.get("/api/persons", (req, res) => {
 app.get("/info", (req, res) => {
     const numOfPeople = persons.length;
     const time = new Date();
-    // console.log(numOfPeople, time);
 
     const infoPage = 
         `<p>Phonebook has info for ${numOfPeople} people</p>
         ${time}`;
 
     res.send(infoPage);
+});
+
+app.post("/api/persons", (req, res) => {
+    let newPerson = req.body;
+    if (!newPerson.name || newPerson.name.length === 0) {
+        return res.status(400).json({error: "Missing name."});
+    }
+    else if (!newPerson.number || newPerson.number.length === 0) {
+        return res.status(400).json({error: "Missing number."});
+    }
+
+    const exists = persons.find(person => person.name === newPerson.name);
+    if (exists) {
+        return res.status(400).json({error: `Person with the name ${exists.name} is already in the list` });
+    }
+
+    const id = generateId();
+    newPerson.id = id;
+
+    persons = persons.concat(newPerson);
+    
+    res.json(newPerson);
 });
 
 app.listen(PORT, () => {
