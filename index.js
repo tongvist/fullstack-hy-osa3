64c8cb3677d4684be307c4ba6c2ee34986c175bc
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-20 15:16:31 +0200

    Tehtävä 3.1

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+
+let persons = [
+      {
+        name: "Arto Hellas",
+        number: "040-9876543",
+        id: 1
+      },
+      {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+      },
+      {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+      },
+      {
+        name: "Mary Poppendick",
+        number: "39-23-6423122",
+        id: 3
+      }
+    ]
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
