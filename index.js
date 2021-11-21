b160ebd52992f2c0f44fec671a80ef5ab894a9ee
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-21 11:50:02 +0200

    Add ui builddirectory and express.static()

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 
 // Attach a 'data' token to POST requests to log the request body
 // Returns a function with the name of the first parameter
