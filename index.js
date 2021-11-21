ee836092d12c053b05435cfcdad28194599bbc63
niku_p <67380624+tongvist@users.noreply.github.com>
2021-11-21 21:15:08 +0200

    Osa 3, tehtävä 3.15

--- index.js
+++ index.js
@@ -71,9 +71,14 @@ const generateId = () => {
 }
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-    res.status(204).end();
+    const id = req.params.id;
+
+    Person.findByIdAndRemove(id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(error => console.log("Error removing document:", error.message));
+
 });
 
 app.get("/api/persons/:id", (req, res) => {
